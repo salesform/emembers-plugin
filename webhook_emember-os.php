198b7286399937520834882ef02b43a2ba6f03ea
kendipeti <72387496+kendipeti@users.noreply.github.com>
2020-10-05 12:54:15 +0200

    Update webhook_emember-os.php

--- webhook_emember-os.php
+++ webhook_emember-os.php
@@ -91,12 +91,12 @@ function posteMember($url,$postdata) {
 	curl_setopt($ch, CURLOPT_URL,$url);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
 	curl_setopt($ch, CURLOPT_POST,1);
-    curl_setopt($ch, CURLOPT_POSTFIELDS,http_build_query($postdata));
-	curl_setopt($ch, CURLOPT_USERAGENT, "SF WebHook");
+    	curl_setopt($ch, CURLOPT_POSTFIELDS,http_build_query($postdata));
+	curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 6.2; WOW64; rv:17.0) Gecko/20100101 Firefox/17.0');
+	curl_setopt($ch, CURLOPT_REFERER, 'https://salesform.hu/');
 	$response=curl_exec ($ch); 
 	curl_close ($ch);
 	return $response;
 }
-
 		
 ?>
